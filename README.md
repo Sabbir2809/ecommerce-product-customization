e0e3c708a400d695029e180dd5223e522a8846db
Md Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2026-02-01 21:53:55 +0600

    chore: initial Next.js setup with TypeScript & Tailwind CSS

---
+++ README.md
@@ -0,0 +1,33 @@
+# E-Commerce Product Customization System
+
+E-Commerce Product Customization System with multi-step variant selection, real-time 3D preview, dynamic pricing and advanced cart management.
+
+## Features
+
+## Technology Stack
+
+- **Programming Language:** TypeScript
+- **Web Framework:** Next.js
+- **State Management:** Zustand
+- **Packages:**
+
+## Project Setup Instructions
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Sabbir2809/ecommerce-product-customization
+   cd ecommerce-product-customization
+   ```
+
+2. Install dependencies:
+
+   ```bash
+   yarn install # npm install
+   ```
+
+3. Run the development server:
+   ```bash
+   yarn dev
+   ```
+4. Open `http://localhost:3000` in your browser.
---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "E-commerce Product Customization",
+  description:
+    "E-Commerce Product Customization System with multi-step variant selection, real-time 3D preview, dynamic pricing and advanced cart management.",
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<Props>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
